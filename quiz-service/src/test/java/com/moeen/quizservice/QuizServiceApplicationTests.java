d624ce54ea5e0ad16cf67bc9d744672e0a2d28ce
moeen775 <moeenahmmed2015br@gmail.com>
2023-12-28 23:51:56 +0600

    quiz service

---
+++ quiz-service/src/test/java/com/moeen/quizservice/QuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moeen.quizservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
